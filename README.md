0ab364c3c79eb0c1e92289d81ec55a7de1e95627
Saksham Bassi <sakshambassi@gmail.com>
2022-04-17 17:48:12 -0400

    adding dragonnet

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # CharizardNet
 Treatment Effects improved with new feature maps 
 
-This work is inspired by Claudia Shi et. al. https://arxiv.org/abs/1906.02120; https://github.com/claudiashi57/dragonnet
+This work is inspired by Claudia Shi et. al. https://arxiv.org/abs/1906.02120; https://github.com/claudiashi57/dragonnet and https://github.com/taki0112/vit-tensorflow
+
+Image classification with Vision Transformer: https://keras.io/examples/vision/image_classification_with_vision_transformer/
--- transformer/ViT.py
+++ transformer/ViT.py
@@ -1,127 +1,177 @@
-import torch
-from torch import nn
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import Sequential
+import tensorflow.keras.layers as nn
 
+from tensorflow import einsum
 from einops import rearrange, repeat
-from einops.layers.torch import Rearrange
-
-# helpers
+from einops.layers.tensorflow import Rearrange
 
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-# classes
+class PreNorm(Layer):
+    def __init__(self, fn):
+        super(PreNorm, self).__init__()
 
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
+        self.norm = nn.LayerNormalization()
         self.fn = fn
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
 
-class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
-    def forward(self, x):
-        return self.net(x)
+    def call(self, x, training=True):
+        return self.fn(self.norm(x), training=training)
 
-class Attention(nn.Module):
-    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
-        super().__init__()
-        inner_dim = dim_head *  heads
+class MLP(Layer):
+    def __init__(self, dim, hidden_dim, dropout=0.0):
+        super(MLP, self).__init__()
+
+        def GELU():
+            def gelu(x, approximate=False):
+                if approximate:
+                    coeff = tf.cast(0.044715, x.dtype)
+                    return 0.5 * x * (1.0 + tf.tanh(0.7978845608028654 * (x + coeff * tf.pow(x, 3))))
+                else:
+                    return 0.5 * x * (1.0 + tf.math.erf(x / tf.cast(1.4142135623730951, x.dtype)))
+
+            return nn.Activation(gelu)
+
+        self.net = Sequential([
+            nn.Dense(units=hidden_dim),
+            GELU(),
+            nn.Dropout(rate=dropout),
+            nn.Dense(units=dim),
+            nn.Dropout(rate=dropout)
+        ])
+
+    def call(self, x, training=True):
+        return self.net(x, training=training)
+
+class Attention(Layer):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0.0):
+        super(Attention, self).__init__()
+        inner_dim = dim_head * heads
         project_out = not (heads == 1 and dim_head == dim)
 
         self.heads = heads
         self.scale = dim_head ** -0.5
 
-        self.attend = nn.Softmax(dim = -1)
-        self.dropout = nn.Dropout(dropout)
+        self.attend = nn.Softmax()
+        self.to_qkv = nn.Dense(units=inner_dim * 3, use_bias=False)
 
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        if project_out:
+            self.to_out = [
+                nn.Dense(units=dim),
+                nn.Dropout(rate=dropout)
+            ]
+        else:
+            self.to_out = []
 
-        self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, dim),
-            nn.Dropout(dropout)
-        ) if project_out else nn.Identity()
+        self.to_out = Sequential(self.to_out)
 
-    def forward(self, x):
-        qkv = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+    def call(self, x, training=True):
+        qkv = self.to_qkv(x)
+        qkv = tf.split(qkv, num_or_size_splits=3, axis=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
 
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
-
+        # dots = tf.matmul(q, tf.transpose(k, perm=[0, 1, 3, 2])) * self.scale
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
         attn = self.attend(dots)
-        attn = self.dropout(attn)
 
-        out = torch.matmul(attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
+        # x = tf.matmul(attn, v)
+        x = einsum('b h i j, b h j d -> b h i d', attn, v)
+        x = rearrange(x, 'b h n d -> b n (h d)')
+        x = self.to_out(x, training=training)
 
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
-            ]))
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
         return x
 
-class ViT(nn.Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
-        super().__init__()
+class Transformer(Layer):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.0):
+        super(Transformer, self).__init__()
+
+        self.layers = []
+
+        for _ in range(depth):
+            self.layers.append([
+                PreNorm(Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                PreNorm(MLP(dim, mlp_dim, dropout=dropout))
+            ])
+
+    def call(self, x, training=True):
+        for attn, mlp in self.layers:
+            x = attn(x, training=training) + x
+            x = mlp(x, training=training) + x
+
+        return x
+
+class ViT(Model):
+    def __init__(self, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim,
+                 pool='cls', dim_head=64, dropout=0.0, emb_dropout=0.0):
+        """
+            image_size: int.
+            -> Image size. If you have rectangular images, make sure your image size is the maximum of the width and height
+            patch_size: int.
+            -> Number of patches. image_size must be divisible by patch_size.
+            -> The number of patches is: n = (image_size // patch_size) ** 2 and n must be greater than 16.
+            num_classes: int.
+            -> Number of classes to classify.
+            dim: int.
+            -> Last dimension of output tensor after linear transformation nn.Linear(..., dim).
+            depth: int.
+            -> Number of Transformer blocks.
+            heads: int.
+            -> Number of heads in Multi-head Attention layer.
+            mlp_dim: int.
+            -> Dimension of the MLP (FeedForward) layer.
+            dropout: float between [0, 1], default 0..
+            -> Dropout rate.
+            emb_dropout: float between [0, 1], default 0.
+            -> Embedding dropout rate.
+            pool: string, either cls token pooling or mean pooling
+        """
+        super(ViT, self).__init__()
+
         image_height, image_width = pair(image_size)
         patch_height, patch_width = pair(patch_size)
 
         assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
 
         num_patches = (image_height // patch_height) * (image_width // patch_width)
-        patch_dim = channels * patch_height * patch_width
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
-            nn.Linear(patch_dim, dim),
-        )
+        self.patch_embedding = Sequential([
+            Rearrange('b (h p1) (w p2) c -> b (h w) (p1 p2 c)', p1=patch_height, p2=patch_width),
+            nn.Dense(units=dim)
+        ], name='patch_embedding')
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-        self.dropout = nn.Dropout(emb_dropout)
+        self.pos_embedding = tf.Variable(initial_value=tf.random.normal([1, num_patches + 1, dim]))
+        self.cls_token = tf.Variable(initial_value=tf.random.normal([1, 1, dim]))
+        self.dropout = nn.Dropout(rate=emb_dropout)
 
         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
         self.pool = pool
-        self.to_latent = nn.Identity()
 
-        self.mlp_head = nn.Sequential(
-            nn.LayerNorm(dim),
-            nn.Linear(dim, num_classes)
-        )
+        self.mlp_head = Sequential([
+            nn.LayerNormalization(),
+            nn.Dense(units=num_classes)
+        ], name='mlp_head')
 
-    def forward(self, img):
-        x = self.to_patch_embedding(img)
-        b, n, _ = x.shape
+    def call(self, img, training=True, **kwargs):
+        x = self.patch_embedding(img)
+        b, n, d = x.shape
 
-        cls_tokens = repeat(self.cls_token, '1 n d -> b n d', b = b)
-        x = torch.cat((cls_tokens, x), dim=1)
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b=b)
+        x = tf.concat([cls_tokens, x], axis=1)
         x += self.pos_embedding[:, :(n + 1)]
-        x = self.dropout(x)
+        x = self.dropout(x, training=training)
 
-        x = self.transformer(x)
+        x = self.transformer(x, training=training)
 
-        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+        if self.pool == 'mean':
+            x = tf.reduce_mean(x, axis=1)
+        else:
+            x = x[:, 0]
 
-        x = self.to_latent(x)
-        return self.mlp_head(x)
+        x = self.mlp_head(x)
+
+        return x
